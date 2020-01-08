890d25b69fb20b50ac4bc6cf641f69ba75fc57e3
Miguel Fernandez <miguelrf95@gmail.com>
2020-01-08 14:52:17 +0000

    Tests with scriptable objects

---
+++ ScriptableObjects/ScriptableObjects_Test/Assets/Scripts/ScriptableObject/LevelSpec.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "LevelSpec")]
+public class LevelSpec : ScriptableObject
+{
+    [SerializeField]
+    private Vector3[] enemySpawnPoints = null;
+
+    public IEnumerable<Vector3> EnemySpawnPoints => enemySpawnPoints;
+}
