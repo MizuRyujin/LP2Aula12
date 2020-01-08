e5302c2c071375df59a236b9fe0345180eead06b
Miguel Fernandez <miguelrf95@gmail.com>
2020-01-08 15:14:34 +0000

    Tests with singleton pattern

---
+++ LazySingleFloat/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LazySingleFloat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SetValue();
+            PrintValue();
+        }
+
+        static void SetValue()
+        {
+            Singleton.Instance.FloatValue = 20;
+        }
+
+        static void PrintValue()
+        {
+            Console.WriteLine(Singleton.Instance.FloatValue);
+        }
+    }
+}
---
+++ LazySingleFloat/Singleton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LazySingleFloat
+{
+    public sealed class Singleton
+    {
+        private static Lazy<Singleton> instance =
+            new Lazy<Singleton>(() => new Singleton());
+
+        public static Singleton Instance => instance.Value;
+
+        public float FloatValue { get; set; }
+
+        private Singleton()
+        { }
+
+    }
+}
